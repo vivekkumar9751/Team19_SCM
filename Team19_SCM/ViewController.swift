81b9bcc9c0c27499b52779ff459ffc08fb151f68
Akshay <manasshukla876@gmail.com>
2024-12-02 11:59:20 +0530

    Initial Commit

---
+++ Team19_SCM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team19_SCM
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
